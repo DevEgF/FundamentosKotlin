fdd83bc50896d1ff878c944cb1cb8f3650ed0be1
Egito Filho <egitofilho4@gmail.com>
2024-11-21 21:30:08 -0300

    desafio_01_gasolina_etanol

---
+++ src/desafios/01_gasolina_etanol/Desafio01_GasolinaEtanol.kt
@@ -0,0 +1,27 @@
+package desafios
+
+fun main() {
+
+    println("Insira o preço por litro da GASOLINA:")
+    println("R$ ")
+    val precoGasolina = readlnOrNull()?.toDoubleOrNull()
+
+    println("Insira o preço por litro do ETANOL:")
+    println("R$ ")
+    val precoEtanol = readlnOrNull()?.toDoubleOrNull()
+
+    if (precoGasolina == null || precoEtanol == null) {
+        println("Os valores inseridos são inválidos.")
+        return
+    }
+
+    val razaoCombustivel = precoEtanol / precoGasolina
+
+    println(
+        when {
+            razaoCombustivel < 0.7 -> "Etanol é mais econômico."
+            razaoCombustivel > 0.7 -> "Gasolina é mais econômica."
+            else -> "Ambos os combustíveis têm o mesmo custo benefício."
+        }
+    )
+}
