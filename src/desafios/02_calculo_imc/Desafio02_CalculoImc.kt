d84ec9b1f6ae24650f1a8e2bf0a47cfcda503afe
Egito Filho <egitofilho4@gmail.com>
2024-11-22 19:57:04 -0300

    desafio_02_calculo_imc

--- src/desafios/02_calculo_imc/Desafio02_CalculoImc.kt
+++ src/desafios/02_calculo_imc/Desafio02_CalculoImc.kt
@@ -1,5 +1,6 @@
 package desafios.`02_calculo_imc`
 
+import desafios.`02_calculo_imc`.ClassificacaoIMC.Companion.gerarClassificacao
 import kotlin.math.pow
 
 fun validarPeso(peso: Double?): Boolean =
@@ -24,6 +25,29 @@ fun classificaImc(imc: Double): String {
     }
 }
 
+enum class ClassificacaoIMC(val descricao: String) {
+    DESCONHECIDO("Desconhecido"),
+    ABAIXO_DO_PESO("Abaixo do peso"),
+    PESO_NORMAL("Peso normal"),
+    SOBREPESO("Sobrepeso"),
+    OBESIDADE_GRAU_I( "Obesidade grau I"),
+    OBESIDADE_GRAU_II("Obesidade grau II"),
+    OBESIDADE_GRAU_III( "Obesidade grau III");
+
+    companion object {
+        fun gerarClassificacao(imc: Double): ClassificacaoIMC {
+            return when {
+                imc < 18.5 -> ABAIXO_DO_PESO
+                imc in 18.5..24.9 -> PESO_NORMAL
+                imc in 25.0..29.9 -> SOBREPESO
+                imc in 30.0..34.9 -> OBESIDADE_GRAU_I
+                imc in 35.0..39.9 -> OBESIDADE_GRAU_II
+                imc > 39.9 -> OBESIDADE_GRAU_III
+                else -> DESCONHECIDO
+            }
+        }
+    }
+}
 
 fun main() {
 
@@ -48,8 +72,8 @@ fun main() {
 
     val imc = calculaImc(peso = peso!!, altura = altura!!)
 
-    val classificaçãoIMC = classificaImc(imc = imc)
+    val classificaçãoIMC = gerarClassificacao(imc = imc)
 
-    println("O seu IMC é ${"%.1f".format(imc)} e a sua classificação é: '${classificaçãoIMC}'.")
+    println("O seu IMC é ${"%.1f".format(imc)} e a sua classificação é: '${classificaçãoIMC.descricao}'.")
 
 }
